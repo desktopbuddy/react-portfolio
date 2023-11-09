36846319a49587235574bac3c9540029e7c3d239
desktopbuddy <hanchondo21@gmail.com>
2023-11-09 02:03:15 -0800

    header and main section setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <header>
+      <div class="header-title">Huaquin Anchondo</div>
+      <nav>
+        <div class="nav-item">
+          <a href="#">About Me</a>
+        </div>
+        <div class="nav-item">
+          <a href="#">Portfolio</a>
+        </div>
+        <div class="nav-item">
+          <a href="#">Contact</a>
+        </div>
+        <div class="nav-item">
+          <a href="#">Resume</a>
+        </div>
+      </nav>
+    </header>
+  )
+}
