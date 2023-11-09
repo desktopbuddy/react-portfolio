83f9dfefc67a8459998f7176b7565889b036e4dc
desktopbuddy <hanchondo21@gmail.com>
2023-11-09 15:09:38 -0800

    about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,12 @@
 export default function About() {
   return (
-    <div>about page..</div>
+    <section id="about-section">
+      <img id="about-img" src="/aba.jpg" alt="Picture of my cat Aba" />
+      <div id="about-text">
+        <p>Hi!! I'm Huaquin.</p>
+        <p>I'm currently learning full-stack web development.</p>
+        <p>Check out my projects!</p>
+      </div>
+    </section>
   )
 }
