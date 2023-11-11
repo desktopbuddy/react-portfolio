65b4e1e42bc2a19336f433f3a757a1480a370d2b
desktopbuddy <hanchondo21@gmail.com>
2023-11-10 21:18:56 -0800

    style portfolio page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,10 @@ export default function Project({ project }) {
   return (
     <div class="project-card">
       <h3 class="project-title">{project.title}</h3>
-      <p>{project.description}</p>
+      <p class="project-description">{project.description}</p>
       <div class="project-links">
-        <button onClick={`location.href=${project.deployed}`}>Live site</button>
-        <button onClick={`location.href=${project.github}`}>GitHub repo</button>
+        <button class="live-btn" onClick={`location.href=${project.deployed}`}>Live site 🡕 </button>
+        <button class="github-btn" onClick={`location.href=${project.github}`}>GitHub</button>
       </div>
     </div>
   )
