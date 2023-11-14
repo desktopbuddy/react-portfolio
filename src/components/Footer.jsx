fa78e1085ac0aaef3523450db253681a7444dc1f
desktopbuddy <hanchondo21@gmail.com>
2023-11-13 23:12:56 -0800

    footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer>
-      footer
+      <a href="github.com/desktopbuddy">GitHub</a>
+      <span>|</span>
+      <a href="linkedin.com">LinkedIn</a>
+      <span>|</span>
+      <a href="https://anilist.co/user/bugsandcritters">AniList</a>
     </footer>
   )
 }
